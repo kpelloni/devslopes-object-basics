337e56afcc9f67d46fc01bcc0af41328c7d338d7
kpelloni <krystine.stanke@gmail.com>
2025-09-21 15:26:30 -0600

    first commit, exercise 1 done

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,34 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+
+const laBellaVita = {
+    name: "La Bella Vita",
+    cuisines: ["Italian", "American", "Alcohol"], 
+    numberOfStars: 4,
+    favorited: true,
+}
+
+laBellaVita.address = "123 Northpark Ave";
+laBellaVita.zipcode = 80915;
+laBellaVita.acceptsReservations = true;
+
+laBellaVita.numberOfStars = 5;
+laBellaVita.favorited = false;
+laBellaVita.cuisines.push("French");
+
+function retrieveProperty(obj, key) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        return obj[key];
+    }
+    return "The information you requested does not exist.";
+}
+
+// Test invocations requested by the exercise:
+console.log('cuisines ->', retrieveProperty(laBellaVita, 'cuisines'));
+console.log('favorited ->', retrieveProperty(laBellaVita, 'favorited'));
+console.log('name ->', retrieveProperty(laBellaVita, 'name'));
+console.log("nickName ->", retrieveProperty(laBellaVita, 'nickName'));
+console.log('state ->', retrieveProperty(laBellaVita, 'state'));
+
+
